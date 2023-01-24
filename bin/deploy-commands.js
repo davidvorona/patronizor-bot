a97e61d198fe1063eb0ff0cf32ae2d5adeb393c3
David Vorona <davidavorona@gmail.com>
2023-01-23 20:58:26 -0800

    Switch to using global commands in script

--- bin/deploy-commands.js
+++ bin/deploy-commands.js
@@ -1,16 +1,16 @@
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 const commands = require("../config/commands");
-const { CLIENT_ID, GUILD_ID } = require("../config/config.json");
+const { CLIENT_ID } = require("../config/config.json");
 const { TOKEN } = require("../config/auth.json");
 
 const rest = new REST({ version: "9" }).setToken(TOKEN);
 
 (async () => {
     try {
-        console.log("Started refreshing guild application (/) commands.");
+        console.log("Started refreshing application (/) commands.");
         await rest.put(
-            Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+            Routes.applicationCommands(CLIENT_ID),
             { body: commands }
         );
         console.log("Successfully reloaded application (/) commands.");
